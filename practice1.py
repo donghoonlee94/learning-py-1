d41c7f760088354375a197705771c2a0b9b6b4a5
donghoon.lee <summarhoony@nurimedia.co.kr>
2025-10-31 19:10:02 +0900

    feat: operator

---
+++ practice1.py
@@ -0,0 +1,45 @@
+print(1+1) # 2
+print(3-2) # 1
+print(5*2) # 10
+print(6/3) # 2.0
+
+# 2의 3승
+print(2**3) # 8
+print(2**4) # 16
+
+# 5를 3으로 나눈 나머지
+print(5%3) # 2
+
+# 5를 3으로 나눈 몫
+print(5//3) # 1
+print(12//3) # 4
+
+print(10>3) # True
+print(4>=7) # False
+print(10<3) # False
+print(5<=5) # True
+print(3==3) # True 
+print(not(3 != 3)) # False
+
+print(3 >2 and 3 < 4) # True
+print(3 >2 & 3 < 4) # True
+print(3 >2 or 3 < 4) # True
+print(3 >2 | 3 < 4) # True
+print(not(3 >2)) # False
+print(not(3 < 2)) # True
+print(not(3 == 3)) # False
+print(not(3 != 3)) # True
+print(not(3 > 2)) # False
+print(not(3 < 2)) # True
+print(not(3 == 3)) # False
+print(not(3 != 3)) # True
+print(3 is 3) # True
+print(3 is not 3) # False
+print(3 in [1, 2, 3]) # True
+print(3 not in [1, 2, 3]) # False
+print(3 in (1, 2, 3)) # True
+print(3 not in (1, 2, 3)) # False
+print(3 in {1, 2, 3}) # True
+print(3 not in {1, 2, 3}) # False
+print(3 in {1: 2, 2: 3, 3: 4}) # True
+print(3 not in {1: 2, 2: 3, 3: 4}) # False
