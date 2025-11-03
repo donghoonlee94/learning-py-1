f898ebe58adf4aa915f72775f972538e45411709
donghoon.lee <summarhoony@nurimedia.co.kr>
2025-11-03 14:05:38 +0900

    feat: 형변환

---
+++ practice14.py
@@ -0,0 +1,25 @@
+# 자료구조의 변경
+# 커피숍
+menu = {"커피", "우유", "주스"}
+print(menu, type(menu))
+
+menu = list(menu)
+print(menu, type(menu))
+
+menu = tuple(menu)
+print(menu, type(menu))
+
+# menu = dict(menu)
+# print(menu, type(menu))
+
+menu = set(menu)
+print(menu, type(menu))
+
+# cabinet = {3: "유재석", 100: "김태호"}
+# print(type(cabinet))
+
+# list = [1,2,3]
+# print(list, type(list))
+
+# tuple = (1,2,3)
+# print(tuple, type(tuple))
