f54860f9377784b781c3a791a12ab87533dcf1ac
donghoon.lee <summarhoony@nurimedia.co.kr>
2025-11-03 19:08:55 +0900

    feat: with, pickle

---
+++ practice25.py
@@ -0,0 +1,12 @@
+import pickle
+
+
+# pickle 로 저장된 파일 불러오기 close 해줄 필요 없음
+with open("profile.pickle", "rb") as profile_file:
+    print(pickle.load(profile_file))
+
+# with open("study.txt", "w", encoding="utf8") as study_file:
+#     study_file.write("파이썬을 열심히 공부하고 있습니다.")
+
+with open("study.txt", "r", encoding="utf8") as study_file:
+    print(study_file.read())
