e29895d783db73ef39b9e2a8c76dd79d5b114bb1
donghoonlee <94donghoon.lee@gmail.com>
2025-11-04 19:24:36 +0900

    feat: 다중상속

--- practice27.py
+++ practice27.py
@@ -18,3 +18,20 @@ class AttackUnit(Unit):
         print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
         if self.hp <= 0:
             print("{0} : 파괴되었습니다.".format(self.name))
+
+
+class Flyable:
+    def __init__(self, flying_speed: int):
+        self.flying_speed = flying_speed
+
+    def fly(self, name, location):
+        print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]"\
+            .format(name, location, self.flying_speed))
+
+class FlyableAttackUnit(AttackUnit, Flyable):
+    def __init__(self, name, hp, damage, flying_speed):
+        AttackUnit.__init__(self, name, hp, damage)
+        Flyable.__init__(self, flying_speed)
+
+valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
+valkyrie.fly(valkyrie.name, "3시")
