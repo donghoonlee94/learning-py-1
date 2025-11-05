4048aafeb174bbc5b0b8d2569b5f9658a71ea4b4
donghoon.lee <summarhoony@nurimedia.co.kr>
2025-11-05 18:47:46 +0900

    feat: *

--- practice36.py
+++ practice36.py
@@ -6,10 +6,17 @@
 # trip_to = travel.vietnam.VietnamPackage()
 # trip_to.detail()
 
-from travel.thailand import ThailandPackage
-trip_to = ThailandPackage()
+# from travel.thailand import ThailandPackage
+# trip_to = ThailandPackage()
+# trip_to.detail()
+
+# from travel.vietnam import VietnamPackage
+# trip_to = VietnamPackage()
+# trip_to.detail()
+
+from travel import *
+trip_to = vietnam.VietnamPackage()
 trip_to.detail()
 
-from travel.vietnam import VietnamPackage
-trip_to = VietnamPackage()
+trip_to = thailand.ThailandPackage()
 trip_to.detail()
