5e88747b786381bf2d3b47d9b427a02d27799cec
donghoon.lee <summarhoony@nurimedia.co.kr>
2025-11-05 17:57:55 +0900

    feat: throw error

---
+++ practice32.py
@@ -0,0 +1,11 @@
+try:
+    print("한 자리 숫자 나누기 전용 계산기")
+    num1 = int(input("첫 번째 숫자를 입력하세요 : "))
+    num2 = int(input("두 번째 숫자를 입력하세요 : "))
+    if num1 >= 10 or num2 >= 10:
+        raise ValueError
+    print("{0} / {1} = {2}".format(num1, num2, num1/num2))
+except ValueError:
+    print("잘못된 값을 입력하였습니다. 한 자리 숫자만 입력하세요.")
+finally:
+    print("계산기를 이용해주셔서 감사합니다.")
