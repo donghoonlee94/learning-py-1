1732249f50bc17940e1c35487e444ef3e47ff500
donghoon.lee <summarhoony@nurimedia.co.kr>
2025-11-05 18:27:59 +0900

    feat: import

---
+++ practice35.py
@@ -0,0 +1,10 @@
+# import practice34 as p34
+# p34.price(3)
+# p34.price_morning(4)
+# p34.price_soldier(5)
+
+from practice34 import price, price_morning, price_soldier as ps
+price(3)
+price_morning(4)
+ps(5)
+
