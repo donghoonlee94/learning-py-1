22d9288552d40d4938fc35817342757fc644bbcc
donghoon.lee <summarhoony@nurimedia.co.kr>
2025-11-05 19:19:13 +0900

    feat: 내장함수

---
+++ practice38.py
@@ -0,0 +1,15 @@
+# # input : 사용자 입력을 받는 함수   
+# language = input("어떤 언어를 좋아하세요? ")
+# print(f"{language}은 아주 좋은 언어입니다!")
+
+# print(dir())
+
+# import bs4
+
+# print(dir(bs4))
+
+# lst = [1, 2, 3]
+# print(dir(lst))
+
+name = "Kim"
+print(dir(name))
