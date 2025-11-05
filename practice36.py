86cd59ea3acc3f1c0957f4410f137192069a0c60
donghoon.lee <summarhoony@nurimedia.co.kr>
2025-11-05 18:44:09 +0900

    feat: package

---
+++ practice36.py
@@ -0,0 +1,15 @@
+# import travel.thailand
+# trip_to = travel.thailand.ThailandPackage()
+# trip_to.detail()
+
+# import travel.vietnam
+# trip_to = travel.vietnam.VietnamPackage()
+# trip_to.detail()
+
+from travel.thailand import ThailandPackage
+trip_to = ThailandPackage()
+trip_to.detail()
+
+from travel.vietnam import VietnamPackage
+trip_to = VietnamPackage()
+trip_to.detail()
